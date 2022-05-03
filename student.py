da04c6f4d41e6718f2f24621b58c8e55862e017d
Joseph Salerno <102991088+joeysalerno@users.noreply.github.com>
2022-05-03 18:48:17 +0000

    jh

--- student.py
+++ student.py
@@ -94,7 +94,7 @@ class Piggy(PiggyParent):
         self.right(primary=50, counter=50)
         self.fwd
         time.sleep(2)
-        self.turn_by_deg(350)
+        self.turn_by_deg(180)
         self.sleep(1)
         self.fwd()
         time.sleep(2)
