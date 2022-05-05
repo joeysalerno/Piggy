d880d2636aaac0e47e6a11674cd8a5615f43fbdf
Joseph Salerno <102991088+joeysalerno@users.noreply.github.com>
2022-05-05 18:24:04 +0000

    po

--- student.py
+++ student.py
@@ -100,6 +100,10 @@ class Piggy(PiggyParent):
         time.sleep(2)
         self.turn_by_deg(180)
         self.sleep(2)
+        self.fwd
+        time.sleep(2)
+        self.turn_by_deg(180)
+        self.sleep(2)
         
         
     def safe_to_dance(self):
